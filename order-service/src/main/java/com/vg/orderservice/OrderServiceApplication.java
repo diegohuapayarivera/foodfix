a71f9b1092f1e81b83443b95bbf5d03074a50e66
diegohuapayarivera <diego.huapaya@vallegrande.edu.pe>
2021-11-14 18:45:15 -0500

    add microservice eureka-service

--- order-service/src/main/java/com/vg/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/vg/orderservice/OrderServiceApplication.java
@@ -2,11 +2,13 @@ package com.vg.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
