59bfd313d5549b462c707d8108422e3d9550efc8
diegohuapayarivera <diego.huapaya@vallegrande.edu.pe>
2021-11-15 17:10:34 -0500

    JPA problem to solve V1

--- order-service/src/main/java/com/vg/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/vg/orderservice/OrderServiceApplication.java
@@ -2,12 +2,11 @@ package com.vg.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
 public class OrderServiceApplication {
--- sale-service/src/main/java/com/vg/saleservice/SaleServiceApplication.java
+++ sale-service/src/main/java/com/vg/saleservice/SaleServiceApplication.java
@@ -2,10 +2,9 @@ package com.vg.saleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableEurekaClient
 public class SaleServiceApplication {
 
